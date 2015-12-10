b8f1c50e1de6d813d31b949e4d3a5d8d248ee384
Joshua de Lange <joshua.delange@gmail.com>
2015-12-10 14:41:31 -0800

    🎆

--- createLabel.py
+++ createLabel.py
@@ -1,16 +1,23 @@
-import sublime, sublime_plugin
+import sublime, sublime_plugin, re
 
-class createLabel(sublime_plugin.TextCommand):
+class createLabelCommand(sublime_plugin.TextCommand):
 
-	# thank you past joshua
+    # thank you past joshua
     # you let us to this
     # https://github.com/joshuadelange/processSfLabels
 
-	def run(self, edit):
+    def run(self, edit):
 
-		# we might have multiple regions 
-		for region in self.view.sel():
+        print("fire!")
 
-			self.view.replace(edit, region, '{{l."' + self.view.substr(region) + '"}}')
+        # we might have multiple regions 
+        for region in self.view.sel():
 
-			pass
+            label_text = self.view.substr(region)
+            label_camel_case = re.sub(r'\W+', '', ''.join(x for x in label_text.title() if not x.isspace()))[:80]
+
+            self.view.replace(edit, region, '{{l.' + label_camel_case + '}}')
+
+            self.view.insert(edit, self.view.size(), "\n\n" + label_camel_case + " : " + label_text + "\n")
+
+            pass
